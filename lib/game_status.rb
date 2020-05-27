66169190847173c55d947c3300f0775c3c322779
Bailey Davis <bailey.gage.davis@gmail.com>
2020-05-27 15:24:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,13 +29,6 @@ end
 def full?(board)
   i = 0
   
-  loop
-    if position_taken?(board, i) == false
-      return false
-    elsif i > board.length - 1
-      return true
-    else
-      i += 1
-    end
-  end
+  until position_taken?(board, i)
+    
   
