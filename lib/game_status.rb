58cac6921ebbc2a7d7b5264485b56815c517bbc2
Bailey Davis <bailey.gage.davis@gmail.com>
2020-05-27 14:47:02 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,9 +18,9 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combination|
     if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-      return true
+      true
     elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
-      return true
+      true
     end
 
   end
